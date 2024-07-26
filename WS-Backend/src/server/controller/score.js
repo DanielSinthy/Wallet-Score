fe473aeeb09a0ed0be9e133c4bc049d3aca6d02e
schmidko <einfach.schmidt@gmail.com>
2024-07-26 22:35:32 +0200

    Added first version of the json structure

--- WS-Backend/src/server/controller/score.js
+++ WS-Backend/src/server/controller/score.js
@@ -4,7 +4,11 @@ const axios = require('axios');
 
 async function calculateScore(address) {
 
+    let age = null;
+    let balance = null;
     let stakeAddress = null;
+    let poolId = null;
+    let poolName = null;
     try {
         const response = await axios.get('https://cardano-mainnet.blockfrost.io/api/v0/addresses/' + address, {headers: {project_id: "mainnetOWJvpavAGJRhQbSNYHA29SEV5OdPUXqp"}});
         stakeAddress = response.data.stake_address;
@@ -18,12 +22,41 @@ async function calculateScore(address) {
 
     try {
         const response = await axios.get('https://cardano-mainnet.blockfrost.io/api/v0/accounts/' + stakeAddress, {headers: {project_id: "mainnetOWJvpavAGJRhQbSNYHA29SEV5OdPUXqp"}});
-        return response.data;
+        age = response.data.active_epoch;
+        balance = response.data.controlled_amount;
+        poolId = response.data.pool_id;
+        //return response.data;
     } catch (e) {
         console.log(e);
-        
     }
-    
+
+    try {
+        const response = await axios.get('https://cardano-mainnet.blockfrost.io/api/v0//pools/' + poolId + '/metadata', {headers: {project_id: "mainnetOWJvpavAGJRhQbSNYHA29SEV5OdPUXqp"}});
+        poolName = response.data.name;
+        //return response.data;
+    } catch (e) {
+        console.log(e);
+    }
+
+    let output = {
+        age: 100,
+        transactions: 500,
+        balance: 55555,
+        policyIds: 20,
+        stakepool: "Test"
+    };
+
+    if (age) {
+        output.age = age;
+    }
+    if (balance) {
+        output.balance = balance;
+    }
+    if (poolName) {
+        output.stakepool = poolName;
+    }
+
+    return output;
 }
 
 
